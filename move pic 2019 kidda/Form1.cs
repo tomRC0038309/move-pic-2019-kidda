08b9dd97fc65f76c1e5e18a1ff7efaeba5683fd0
Thomas Manville <rc0038309@student.riversidecollege.ac.uk>
2019-09-10 11:09:19 +0100

    Add project files.

---
+++ move pic 2019 kidda/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace move_pic_2019_kidda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
