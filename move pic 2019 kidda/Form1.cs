82f90993ffba84ec28a7e681c8797d98436f1ff1
Thomas Manville <rc0038309@student.riversidecollege.ac.uk>
2019-09-16 15:08:18 +0100

    lad this is epic

--- move pic 2019 kidda/Form1.cs
+++ move pic 2019 kidda/Form1.cs
@@ -24,9 +24,11 @@ namespace move_pic_2019_kidda
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            if (pictureBox1.Location.X + 5 < ClientRectangle.Width)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            }
         }
-
         private void Button2_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
